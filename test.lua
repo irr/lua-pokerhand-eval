ade83b2b7469679a490312a9714835fd50bcadae
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-04-30 10:58:19 -0300

    fmt

--- test.lua
+++ test.lua
@@ -7,37 +7,40 @@ analysis = require "holdem.analysis"
 hand = { card.Card(2, 1), card.Card(2, 2) }
 board = { card.Card(14, 1), card.Card(14, 2), card.Card(13, 3) }
 
-print(">>>>", 2, __.each(hand, function (c) print(c:tostring()) end)
+function d(h, b)
+    local n = #h 
+    if b then n = n + #b end
+    print(string.format(">>>> %d cards", n))
+    local p = function (s, t) 
+        print("  "..s..":")
+        for i, v in pairs(t) do
+            print(string.format("     %d. %s", i, v:tostring()))
+        end
+    end
+    p("hand", h)
+    if b then p("board", b) end
+end
+
+d(hand)
 print(analysis.evaluate(hand, {}))
 
-print()
-
-print(">>>>", 5)
+d(hand, board)
 print(analysis.evaluate(hand, board))
 
-print()
-
-print(">>>>", 6)
+d(hand, board)
 board = { card.Card(10, 2), card.Card(14, 2), card.Card(13, 2), card.Card(7, 2) }
 print(analysis.evaluate(hand, board))
 
-print()
-
-print(">>>>", "7A")
+d(hand, board)
 board = { card.Card(3,3), card.Card(3,1), card.Card(8,4), card.Card(5,4), card.Card(4,4) }
 print(analysis.evaluate(hand, board))
 
-print()
-
-print(">>>>", "7B")
+d(hand, board)
 board = { card.Card(3,3), card.Card(3,1), card.Card(10,4), card.Card(5,4), card.Card(4,4) }
 print(analysis.evaluate(hand, board))
 
-print()
-
-print(">>>>", "7C")
+d(hand, board)
 board = { card.Card(2,3), card.Card(2,4), card.Card(10,4), card.Card(5,4), card.Card(4,4) }
 print(analysis.evaluate(hand, board))
 
 print()
-
