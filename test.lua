f9eba53bae26afa835ab03dd77e72398080a8fa4
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-04-30 11:40:31 -0300

    test-ok

--- test.lua
+++ test.lua
@@ -22,23 +22,20 @@ end
 d(hand)
 print(analysis.evaluate(hand, {}))
 
-d(hand, board)
-print(analysis.evaluate(hand, board))
-
-d(hand, board)
 board = { card.Card(10, 2), card.Card(14, 2), card.Card(13, 2), card.Card(7, 2) }
+d(hand, board)
 print(analysis.evaluate(hand, board))
 
-d(hand, board)
 board = { card.Card(3,3), card.Card(3,1), card.Card(8,4), card.Card(5,4), card.Card(4,4) }
+d(hand, board)
 print(analysis.evaluate(hand, board))
 
-d(hand, board)
 board = { card.Card(3,3), card.Card(3,1), card.Card(10,4), card.Card(5,4), card.Card(4,4) }
+d(hand, board)
 print(analysis.evaluate(hand, board))
 
-d(hand, board)
 board = { card.Card(2,3), card.Card(2,4), card.Card(10,4), card.Card(5,4), card.Card(4,4) }
+d(hand, board)
 print(analysis.evaluate(hand, board))
 
 print()
